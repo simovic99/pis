f0309840ccbd57b006ed59232108ccb6772b90a2
simovic99 <simovic.petar99@gmail.com>
2021-02-11 12:28:53 +0100

    sql update

--- opg.sql
+++ opg.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 29, 2021 at 04:36 PM
+-- Generation Time: Feb 11, 2021 at 12:27 PM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.11
 
@@ -46,20 +46,22 @@ CREATE TABLE `failed_jobs` (
 
 CREATE TABLE `kategorije` (
   `id` int(11) NOT NULL,
-  `naziv` varchar(30) NOT NULL
+  `naziv` varchar(30) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `kategorije`
 --
 
-INSERT INTO `kategorije` (`id`, `naziv`) VALUES
-(1, 'Voće'),
-(2, 'Povrće'),
-(3, 'Med'),
-(4, 'vino'),
-(5, 'rakije'),
-(6, 'Ulja');
+INSERT INTO `kategorije` (`id`, `naziv`, `created_at`, `updated_at`) VALUES
+(1, 'Voće', NULL, NULL),
+(2, 'Povrće', NULL, NULL),
+(3, 'Med', NULL, NULL),
+(4, 'vino', NULL, NULL),
+(5, 'Rakije', NULL, '2021-02-05 14:39:58'),
+(6, 'Ulja', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -94,6 +96,7 @@ CREATE TABLE `opg` (
   `naziv` varchar(30) NOT NULL,
   `telefon` varchar(30) NOT NULL,
   `adresa` varchar(50) NOT NULL,
+  `Lokalitet` text NOT NULL,
   `user_id` bigint(30) UNSIGNED NOT NULL,
   `updated_at` date NOT NULL,
   `created_at` date NOT NULL
@@ -103,10 +106,10 @@ CREATE TABLE `opg` (
 -- Dumping data for table `opg`
 --
 
-INSERT INTO `opg` (`id`, `naziv`, `telefon`, `adresa`, `user_id`, `updated_at`, `created_at`) VALUES
-(1, 'Opg Šimović', '039-840-025', 'Proboj 56, Ljubuški', 1, '2021-01-25', '2021-01-25'),
-(3, 'Opg 2', '31231231', 'Ljubuški', 2, '2021-01-25', '2021-01-25'),
-(4, 'opg 3', '036-123-123', 'Mostar bb', 4, '2021-01-29', '2021-01-26');
+INSERT INTO `opg` (`id`, `naziv`, `telefon`, `adresa`, `Lokalitet`, `user_id`, `updated_at`, `created_at`) VALUES
+(1, 'Opg Šimović', '039-840-025', 'Proboj 56, Ljubuški', 'Ljubuški se nalazi u samom zapadu Hercegovine uz granicu Republike Hrvatske. \r\nUz poznati krški hercegovački reljef Ljubuški ima više plodnih polja. \r\nKlima je umjerena mediteranska s 2.300 sunčanih sati tijekom godine', 1, '2021-02-11', '2021-01-25'),
+(3, 'Opg 2', '31231231', 'Ljubuški', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim vec.', 2, '2021-01-25', '2021-01-25'),
+(4, 'opg 3', '036-123-123', 'Mostar bb', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim vec.', 4, '2021-01-29', '2021-01-26');
 
 -- --------------------------------------------------------
 
@@ -155,7 +158,7 @@ INSERT INTO `products` (`id`, `naziv`, `opis`, `cijena`, `img`, `kategorija_id`,
 (6, 'Travarica', '1l', 10, 'NXttB9z6VQp4imdDXxGc1UcoLwqGQ4GzaKwxmRky.jpg', 5, 3, '2021-01-26 13:31:43', '2021-01-26 13:31:43'),
 (7, 'Maslinovo ulje', '1l ekstra djevičansko', 30, 'QEDfKmsgEDOaaUK0zROrdiL9MyODfHAm2hUVv4QI.jpg', 1, 4, '2021-01-26 13:42:50', '2021-01-28 21:58:55'),
 (9, 'Med', 'kadulja', 10, 'i5maVZz97qLvK22LTXDoQ66bgtxw9qWYDRQvsFaA.png', 3, 3, '2021-01-28 21:55:52', '2021-01-28 21:55:52'),
-(10, 'Loza', '1l', 6, 'X1QHlMmzWE1F5D9lVGh2iT4Abu0cdBmb7SEuIL2J.jpg', 5, 4, '2021-01-28 21:56:47', '2021-01-28 21:56:47');
+(10, 'Loza', '1l', 5.85, 'X1QHlMmzWE1F5D9lVGh2iT4Abu0cdBmb7SEuIL2J.jpg', 1, 4, '2021-01-28 21:56:47', '2021-01-29 15:05:38');
 
 -- --------------------------------------------------------
 
@@ -249,7 +252,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `kategorije`
 --
 ALTER TABLE `kategorije`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `migrations`
@@ -261,7 +264,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `opg`
 --
 ALTER TABLE `opg`
-  MODIFY `id` bigint(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `products`
