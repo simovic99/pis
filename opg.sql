e4f409bb57bc9630e6fcae0e0b04570145d21b92
simovic99 <simovic.petar99@gmail.com>
2021-02-17 23:54:37 +0100

    sql update

--- opg.sql
+++ opg.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 11, 2021 at 12:27 PM
+-- Generation Time: Feb 17, 2021 at 11:53 PM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.11
 
@@ -61,7 +61,8 @@ INSERT INTO `kategorije` (`id`, `naziv`, `created_at`, `updated_at`) VALUES
 (3, 'Med', NULL, NULL),
 (4, 'vino', NULL, NULL),
 (5, 'Rakije', NULL, '2021-02-05 14:39:58'),
-(6, 'Ulja', NULL, NULL);
+(6, 'Ulja', NULL, NULL),
+(8, 'suho voće', '2021-02-17 15:13:30', '2021-02-17 15:13:57');
 
 -- --------------------------------------------------------
 
@@ -108,8 +109,8 @@ CREATE TABLE `opg` (
 
 INSERT INTO `opg` (`id`, `naziv`, `telefon`, `adresa`, `Lokalitet`, `user_id`, `updated_at`, `created_at`) VALUES
 (1, 'Opg Šimović', '039-840-025', 'Proboj 56, Ljubuški', 'Ljubuški se nalazi u samom zapadu Hercegovine uz granicu Republike Hrvatske. \r\nUz poznati krški hercegovački reljef Ljubuški ima više plodnih polja. \r\nKlima je umjerena mediteranska s 2.300 sunčanih sati tijekom godine', 1, '2021-02-11', '2021-01-25'),
-(3, 'Opg 2', '31231231', 'Ljubuški', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim vec.', 2, '2021-01-25', '2021-01-25'),
-(4, 'opg 3', '036-123-123', 'Mostar bb', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim vec.', 4, '2021-01-29', '2021-01-26');
+(3, 'Opg 2', '31231231', 'Ljubuški', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim vec.', 4, '2021-02-17', '2021-01-25'),
+(4, 'opg 3', '036-123-123', 'Mostar bb', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim vec.', 2, '2021-02-17', '2021-01-26');
 
 -- --------------------------------------------------------
 
@@ -142,6 +143,7 @@ CREATE TABLE `products` (
   `opis` text NOT NULL,
   `cijena` float NOT NULL,
   `img` text NOT NULL,
+  `popust` int(11) NOT NULL DEFAULT 0,
   `kategorija_id` int(11) NOT NULL,
   `opg_id` bigint(30) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
@@ -152,13 +154,13 @@ CREATE TABLE `products` (
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`id`, `naziv`, `opis`, `cijena`, `img`, `kategorija_id`, `opg_id`, `created_at`, `updated_at`) VALUES
-(4, 'Grožđe', 'žilavka', 6, 'fAykrISoafvgvGWbeuv3O7YPrpDv4l4KcsISibFY.jpg', 1, 1, '2021-01-26 12:33:54', '2021-01-28 21:07:27'),
-(5, 'Krumpir', 'domaći', 0.5, 'gLzObjzGZaaO0hrWQUy0zyIuR6Ou7votFA0Zv4nW.jpg', 2, 1, '2021-01-26 13:15:05', '2021-01-26 13:15:05'),
-(6, 'Travarica', '1l', 10, 'NXttB9z6VQp4imdDXxGc1UcoLwqGQ4GzaKwxmRky.jpg', 5, 3, '2021-01-26 13:31:43', '2021-01-26 13:31:43'),
-(7, 'Maslinovo ulje', '1l ekstra djevičansko', 30, 'QEDfKmsgEDOaaUK0zROrdiL9MyODfHAm2hUVv4QI.jpg', 1, 4, '2021-01-26 13:42:50', '2021-01-28 21:58:55'),
-(9, 'Med', 'kadulja', 10, 'i5maVZz97qLvK22LTXDoQ66bgtxw9qWYDRQvsFaA.png', 3, 3, '2021-01-28 21:55:52', '2021-01-28 21:55:52'),
-(10, 'Loza', '1l', 5.85, 'X1QHlMmzWE1F5D9lVGh2iT4Abu0cdBmb7SEuIL2J.jpg', 1, 4, '2021-01-28 21:56:47', '2021-01-29 15:05:38');
+INSERT INTO `products` (`id`, `naziv`, `opis`, `cijena`, `img`, `popust`, `kategorija_id`, `opg_id`, `created_at`, `updated_at`) VALUES
+(4, 'Grožđe', 'žilavka', 6, 'fAykrISoafvgvGWbeuv3O7YPrpDv4l4KcsISibFY.jpg', 0, 1, 1, '2021-01-26 12:33:54', '2021-01-28 21:07:27'),
+(5, 'Krumpir', 'domaći', 0.5, 'gLzObjzGZaaO0hrWQUy0zyIuR6Ou7votFA0Zv4nW.jpg', 0, 2, 1, '2021-01-26 13:15:05', '2021-01-26 13:15:05'),
+(6, 'Travarica', '1l', 10, 'NXttB9z6VQp4imdDXxGc1UcoLwqGQ4GzaKwxmRky.jpg', 0, 5, 3, '2021-01-26 13:31:43', '2021-01-26 13:31:43'),
+(9, 'Med', 'kadulja', 10, 'i5maVZz97qLvK22LTXDoQ66bgtxw9qWYDRQvsFaA.png', 20, 3, 3, '2021-01-28 21:55:52', '2021-02-17 17:46:28'),
+(10, 'Loza', '1l', 5.85, 'X1QHlMmzWE1F5D9lVGh2iT4Abu0cdBmb7SEuIL2J.jpg', 10, 5, 4, '2021-01-28 21:56:47', '2021-02-17 16:59:46'),
+(11, 'brusnica', 'domaca', 10, '0piWsxI0uH1gvn7uG2DCPh43zE7ZITlph2utpLW8.jpg', 0, 1, 4, '2021-02-17 15:15:32', '2021-02-17 16:59:36');
 
 -- --------------------------------------------------------
 
@@ -183,7 +185,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `role`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'pero', 'simovic.petar99@gmail.com', NULL, '$2y$10$jMUZl5rnWvFv9wwq2gW9iOOJJqXPypum4er8ecLHdqLK3NZRrmlCq', 2, '7jTRpNVvu5fMRVTNzC3W1JLbd8suZKgMtfxKmxHH2Tua8lk2ICNJlhnTeAxA', '2021-01-25 16:50:19', '2021-01-25 16:50:19'),
+(1, 'pero', 'simovic.petar99@gmail.com', NULL, '$2y$10$jMUZl5rnWvFv9wwq2gW9iOOJJqXPypum4er8ecLHdqLK3NZRrmlCq', 2, 'uCX2IIsxk28oYFyQfnV5Y8s8MzbOF7KJoXmyBphxUfWnF6GMX2I8cT9gJz6g', '2021-01-25 16:50:19', '2021-01-25 16:50:19'),
 (2, 'ante', 'a@a.b', NULL, '$2y$10$jZbbTDabye4DZ8/T32S/zOcjFOa4O2fYsr4dL1oF0el7gVnJ.xt3C', 0, NULL, '2021-01-26 10:47:23', '2021-01-26 10:47:23'),
 (4, 'korisnik', 'user@user.com', NULL, '$2y$10$bEigzVyut7cW3qhdUDwpEuWVgsUlTW.6D/xYuQeKzN6KUN5N.BSiu', 0, NULL, '2021-01-28 23:11:49', '2021-01-28 23:12:29');
 
@@ -252,7 +254,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `kategorije`
 --
 ALTER TABLE `kategorije`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `migrations`
@@ -270,7 +272,7 @@ ALTER TABLE `opg`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `users`
